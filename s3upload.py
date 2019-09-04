01cd68f0eed0f509c4d52b3fba5b8d23342935ae
Tulsidas <tulsi.das1404@gmail.com>
2019-09-04 14:39:36 -0400

    Added Comments

--- s3upload.py
+++ s3upload.py
@@ -1,4 +1,11 @@
+# Import boto3 library for Python.
 import boto3
+
+# Calling the s3 service.
 s3 = boto3.resource('s3')
+
+# Create a s3 bucket.
 s3.create_bucket(Bucket= 'tulsidas-web')
+
+# Create a object to upload a file.
 s3.Object('tulsidas-web','webdata.csv').upload_file(Filename='/home/ubuntu/pandas/webscraping/webdata.csv')
