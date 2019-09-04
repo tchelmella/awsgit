d26b892386c0dee4ab28daff8448d0e94dd6e3e8
Tulsidas Chelmella <32229165+tchelmella@users.noreply.github.com>
2019-09-04 10:47:13 -0400

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,13 @@
+# Import pandas library
 import pandas as pd
+
+# Read the csv file using pandas.
 df = pd.read_csv(
     'https://vincentarelbundock.github.io/Rdatasets/'
     'csv/datasets/OrchardSprays.csv')
 
+# Write the csv file.
 df.to_csv(r'/home/ubuntu/pandas/webscraping/webdata.csv',sep='\t',index=False)
+
+# print the dataframe.
 print(df)
